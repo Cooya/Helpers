52d878171e1116dd6db19b52dcbc1a2cb26f52d9
Nicolas <nico@cooya.fr>
2020-05-08 14:39:55 +0200

    index added for counters

--- counter/src/Counter.js
+++ counter/src/Counter.js
@@ -6,28 +6,29 @@ let counters;
 let cache = {};
 
 module.exports = class {
-	constructor(id) { // private constructor
+	// private constructor
+	constructor(id) {
 		this.id = id;
 	}
 
 	inc() {
-		return counters.updateOne({id: this.id}, {$inc: {value: 1}}, {upsert: true});
+		return counters.updateOne({ id: this.id }, { $inc: { value: 1 } }, { upsert: true });
 	}
 
 	async val() {
-		const counter = await counters.findOne({id: this.id});
+		const counter = await counters.findOne({ id: this.id });
 		return counter ? counter.value : 0;
 	}
 
 	clear() {
-		return counters.deleteOne({id: this.id});
+		return counters.deleteOne({ id: this.id });
 	}
 
 	static async connect(dbUrl) {
-		if(mongoConnection)
-			return;
-		mongoConnection = await MongoClient.connect(dbUrl, {useNewUrlParser: true});
+		if (mongoConnection) return;
+		mongoConnection = await MongoClient.connect(dbUrl, { useNewUrlParser: true });
 		counters = mongoConnection.db().collection('counters');
+		await counters.createIndex({ id: 1 }, { name: 'id_index', unique: true });
 	}
 
 	static async disconnect() {
@@ -37,10 +38,10 @@ module.exports = class {
 	}
 
 	static get(id, options = {}) {
-		if(options.dailyCounter || options.daily) id = id + '-' + dateFormat(new Date(), 'dd-mm-yyyy');
-		else if(options.global) id = id + '-global';
+		if (options.dailyCounter || options.daily) id = id + '-' + dateFormat(new Date(), 'dd-mm-yyyy');
+		else if (options.global) id = id + '-global';
 
-		if(!cache[id]) cache[id] = new this(id);
+		if (!cache[id]) cache[id] = new this(id);
 		return cache[id];
 	}
 
@@ -57,10 +58,10 @@ module.exports = class {
 	}
 
 	static clear(id, options = {}) {
-		if(options.dailyCounter || options.daily) id = id + '-' + dateFormat(new Date(), 'dd-mm-yyyy');
-		else if(options.global) id = id + '-global';
+		if (options.dailyCounter || options.daily) id = id + '-' + dateFormat(new Date(), 'dd-mm-yyyy');
+		else if (options.global) id = id + '-global';
 
-		return counters.deleteOne({id});
+		return counters.deleteOne({ id });
 	}
 
 	static clearAll() {
